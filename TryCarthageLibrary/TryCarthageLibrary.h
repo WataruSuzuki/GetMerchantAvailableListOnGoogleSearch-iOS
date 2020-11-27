db58c69e84f7b089ef8920810ef871b524d30680
WataruSuzuki <wataru0406@hotmail.co.jp>
2020-11-27 22:10:00 +0900

    Created xcode project

---
+++ TryCarthageLibrary/TryCarthageLibrary.h
@@ -0,0 +1,18 @@
+//
+//  TryCarthageLibrary.h
+//  TryCarthageLibrary
+//
+//  Created by watsuzuk on 2021/05/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TryCarthageLibrary.
+FOUNDATION_EXPORT double TryCarthageLibraryVersionNumber;
+
+//! Project version string for TryCarthageLibrary.
+FOUNDATION_EXPORT const unsigned char TryCarthageLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TryCarthageLibrary/PublicHeader.h>
+
+
---
+++ TryCarthageLibrary/TryCarthageLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  TryCarthageLibrary.swift
+//  TryCarthageLibrary
+//
+//  Created by watsuzuk on 2021/05/12.
+//
+
+import Foundation
+
+public class TryCarthageLibrary {
+    public init() {
+        
+    }
+}
